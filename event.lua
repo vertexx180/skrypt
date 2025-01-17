a5ae5cda70fa57ec02157e32942a0c6ddb5fafbe
vertexx180 <vertexx288@gmail.com>
2025-01-17 15:50:44 +0100

    Add files via upload

---
+++ event.lua
@@ -0,0 +1,11 @@
+script_key = "XqMPDhjYwrbdEwiPvmEAcRhJChBOwawx";
+_G.GDO_PETGAMES_EVENT = true
+_G.GDO_CONTROL_ROOM = false
+_G.GPAW_RAFFLE_ENTRIES = 0
+_G.GGFX_MODE = 1
+_G.GZONE_TO = 999 -- to enter event from W3 intead put 999
+_G.GPOTIONS = {"Coins","Lucky","Treasure Hunter","Walkspeed","Diamonds","Damage"}
+_G.GWEBHOOK_USERID = "1300483506383749192"
+_G.GWEBHOOK_LINK = "https://discord.com/api/webhooks/1305952211469336607/pD-bV7LE0YEwpkX92W6L7QYS6HZd8ET9YWSen2Novaup3Gw8uqtcUO9l9iQ9ve1VnUCU"
+_G.GMAIL_RECEIVERS = {"ttdmail2"} -- for Huges
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/34915da4ad87a5028e1fd64efbe3543f.lua"))()
